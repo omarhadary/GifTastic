3f6f5ee2ea7bae45f7fae7b6c72dc0878174dff0
Our Laptop <Our Laptop>
2016-11-03 00:39:13 -0700

    changed gihpy url to work on heroku

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,7 @@
     function displayGif() {
         $(".gifs").empty();
         var state = $(this).data("name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + state + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + state + "&api_key=dc6zaTOxFJmzC&limit=10";
         $.ajax({
                 url: queryURL,
                 method: "GET"
