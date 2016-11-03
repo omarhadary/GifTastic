33669c636d43d3b5805d636b4896d9e84f907c71
Our Laptop <Our Laptop>
2016-11-03 00:35:54 -0700

    finished still vs. animate and add button functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
  $(document).ready(function() {
     // create array of strings in variable called topics
-    var topics = ["California", "New York", "Colorado", "Michigan", "Hawaii", "Alaska", "Montana"];
+    var topics = ["California", "Florida", "New York", "Texas", "Hawaii", "Washington", "Colorado", "Virginia", "North Carolina", "Arizona", "Montana", "Georgia", "Idaho"];
     // create buttons for the topics in the array using a loop that appends each button
     function showButtons() {
+        $(".buttons").empty();
         for (var i = 0; i < topics.length; i++) {
-            // $(".buttons").empty();
             var buttons = $("<button>");
             buttons.addClass("state");
             buttons.attr("data-name", topics[i]);
-            buttons.text(topics[i]);
+            buttons.html(topics[i]);
             $(".buttons").append(buttons);
         }
     };
@@ -18,27 +18,57 @@
         var state = $(this).data("name");
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + state + "&api_key=dc6zaTOxFJmzC&limit=10";
         $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).done(function(response) {
-            console.log(queryURL);
-            console.log(response);
-            var results = response.data;
-            for (var j = 0; j < results.length; j++) {
-                var stateSpan = $("<span class='state-span'>");
-                var stateGif = $("<img>");
-                stateGif.attr("src", results[j].images.fixed_height_still.url);
-                var rating = $("<p>");
-                rating.text("Rating: " + results[j].rating);
-                stateSpan.append(stateGif);
-                stateSpan.append(rating);
-                $(".gifs").prepend(stateSpan);
+                url: queryURL,
+                method: "GET"
+            }).done(function(response) {
+                var results = response.data;
+                for (var j = 0; j < results.length; j++) {
+                    var stateSpan = $("<span class='state-span'>");
+                    var stateGif = $("<img>");
+                    stateGif.attr("src", results[j].images.fixed_height_still.url);
+                    stateGif.addClass("gif");
+                    // assign a still value to the current state of the gif
+                    stateGif.attr("data-state", "still");
+                    // assign the still and animate value to the gif
+                    stateGif.attr("data-still", results[j].images.fixed_height_still.url);
+                    stateGif.attr("data-animate", results[j].images.fixed_height.url);
+                    // create the rating tag
+                    var rating = $("<p class='rating-style'>");
+                    var ratingUpperCase = results[j].rating.toUpperCase();
+                    rating.html("Rating: " + ratingUpperCase);
+                    // prepend the gif + rating to the page
+                    stateSpan.append(stateGif);
+                    stateSpan.append(rating);
+                    $(".gifs").prepend(stateSpan);
+                }
+            })
+            // create on click event handler to switch between still and animate gif state
+        $(document).on("click", ".gif", function() {
+            var state = $(this).attr('data-state');
+            if (state === "still") {
+                $(this).attr("src", $(this).data("animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).data("still"));
+                $(this).attr("data-state", "still");
             }
-        })
+        });
     }
-    $(document).on('click', '.state', displayGif);
+    // create on click event handler to take user input and push to the topics array
+    $("#add-state").on("click", function() {
+            var userInput = $("#state-input").val().trim();
+            var newState = userInput.replace(/\w\S*/g, function(txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            });
+            topics.push(newState);
+            $("#state-input").val("");
+            // run function to show the topics buttons
+            showButtons();
+            // prevents page from refreshing when user submits input
+            return false;
+        })
+        // create on click event handler to display the gift if any of the topic buttons are clicked
+    $(document).on('click', ".state", displayGif);
+    // run function to show the topics buttons
     showButtons();
-    // when image is clicked switch to animated gif, when animated gif is clicked again switch back to still gif
-    // Display rating under every gif
-    // add a form to the page that takes the value from a user input box and adds to topics array
  });
